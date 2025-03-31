d1d7c71d57e0cef0c5e1f31aa067884e49bd1de5
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2025-03-31 20:38:25 -0300

    carga de clase 4

---
+++ Clase-4/script.js
@@ -0,0 +1,12 @@
+function mover() {
+    let parrafo1=document.querySelector(".translate")
+    parrafo1.style.transform="translate(100px,100px)"
+}
+function aplicarAnimacion() {
+    let cubo=document.querySelector("h1");
+    cubo.classList.add("animacionColor")
+}
+const pausar=()=>{
+    let padre=document.querySelector(".padre");
+    padre.style.animation="none"
+}
