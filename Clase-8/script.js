b0bb681c5a8ff775a86b50b31fed3d7c9ab5257c
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2025-04-28 18:51:01 -0300

    clase 8

---
+++ Clase-8/script.js
@@ -0,0 +1,6 @@
+//pliegue y despliegue del navegador
+
+function desplegar(){
+    let botonesTema=document.querySelectorAll(".boton");
+    botonesTema.forEach((btn)=> btn.classList.toggle("show"))
+}
